5470d726461d7868b201abd5fc5e5b09301d53eb
duereg <duereg@hotmail.com>
2012-11-05 17:37:56 +0800

    Formatted the js and put in some jshint comments.

--- spec/boardViewModel.import.spec.js
+++ spec/boardViewModel.import.spec.js
@@ -21,26 +21,26 @@ describe('With a boardViewModel', function() {
 
         it('of size 10x10 with 10 mines, 10 mines positions will be recorded.', function() {
             var testBoard = boardBuilder(10, 10, 10);
-            boardVm.import(testBoard);
+            boardVm.importTiles(testBoard);
             expect(boardVm.minePositions.length).toEqual(10);
         });
 
         it('of size 1x1 with 1 mine, 1 mine position will be recorded.', function() {
             var testBoard = boardBuilder(1, 1, 1);
-            boardVm.import(testBoard);
+            boardVm.importTiles(testBoard);
             expect(boardVm.minePositions.length).toEqual(1);
         });
 
         it('of size 10x10 with 10 mines, a 12x12 tile array will be created.', function() {
             var testBoard = boardBuilder(10, 10, 10);
-            boardVm.import(testBoard);
+            boardVm.importTiles(testBoard);
             expect(boardVm.tiles().length).toEqual(12);
             expect(boardVm.tiles()[0].length).toEqual(12);
         });
 
         it('of size 1x1 with 1 mine, 3x3 tile array will be created.', function() {
             var testBoard = boardBuilder(1, 1, 1);
-            boardVm.import(testBoard);
+            boardVm.importTiles(testBoard);
             expect(boardVm.tiles().length).toEqual(3);
             expect(boardVm.tiles()[0].length).toEqual(3);
         });
